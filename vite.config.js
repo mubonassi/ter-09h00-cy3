6e2af5e00389e63ec25ccc889b8f1768a13c7bb4
mubonassi <mpbonassi@gmail.com>
2025-11-04 10:12:01 -0300

    asdasdasdasd

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/ter-09h00-cy3/",
-  plugins: [react()],
-})
